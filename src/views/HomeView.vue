bd61598704d57d17402f9a87aa4c1e6830f53b03
Andrew Surov <7289680+adsurov@users.noreply.github.com>
2024-09-06 00:17:21 +0100

    Add and configure Element UI library

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,16 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <el-time-select
+      v-model="value"
+      :picker-options="{
+        start: '08:30',
+        step: '00:15',
+        end: '18:30',
+      }"
+      placeholder="Select time"
+    >
+    </el-time-select>
   </div>
 </template>
 
@@ -9,10 +19,15 @@
 import Vue from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-export default Vue.extend({
+export default {
   name: "HomeView",
   components: {
     HelloWorld,
   },
-});
+  data() {
+    return {
+      value: "",
+    };
+  },
+};
 </script>
